f4100c158b8d3e3c4de75214ba938449f9ee39e7
Max Kuzenko <mkuzenko@auditboard.com>
2025-07-15 17:35:15 -0700

    parser

--- utils/parser.py
+++ utils/parser.py
@@ -21,3 +21,33 @@ class SummaryParser:
             ticket_data["description"] = parts[2]
 
         return ticket_data
+
+
+class FieldMapper:
+    def __init__(self):
+        self.issue_type = "Bug"  # Default issue type for Jira tickets
+
+    def map_fields(self, ticket_data: dict) -> dict:
+        """
+        Maps the parsed ticket data to the fields required by the Jira API.
+
+        Args:
+            ticket_data (dict): The parsed ticket data.
+
+        Returns:
+            dict: A dictionary containing the mapped fields.
+        """
+
+        project = ticket_data.get("project")
+        feature = ticket_data.get("feature")
+        summary = ticket_data.get("description")
+
+        final_summary = f"[{project} | {feature}]: {summary}"
+
+        team_name = f"PED: {project}"
+
+        return {
+            "summary": final_summary,
+            #"team": team_name,
+            "issue_type": self.issue_type,
+        }
